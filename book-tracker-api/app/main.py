59fdbe7015dad67e60ba7c58fbf9f2e0c1ecb913
Aaron Boakye <cheminko@hotmail.com>
2026-08-15 21:49:06 +0100

    All CRUD endpoints added

--- book-tracker-api/app/main.py
+++ book-tracker-api/app/main.py
@@ -8,11 +8,13 @@ from app.schemas import BookResponse
 from app.schemas import BookCreate
 
 
+
 app = FastAPI()
 
 @app.get("/books", response_model=list[BookResponse])
 def get_books(db: Session = Depends(get_db)):
     return db.query(Book).all()
+
 
 @app.post("/books", response_model=BookResponse, status_code=201)
 def create_book(book: BookCreate, db: Session = Depends(get_db)):
@@ -21,3 +23,36 @@ def create_book(book: BookCreate, db: Session = Depends(get_db)):
     db.commit()
     db.refresh(new_book)
     return new_book
+
+
+@app.get("/books/{book_id}", response_model=BookResponse)
+def get_book(book_id: int, db: Session = Depends(get_db)):
+    book = db.query(Book).filter(Book.id == book_id).first()
+    if not book:
+        raise HTTPException(status_code=404, detail="Book not found")
+    return book
+
+
+@app.put("/books/{book_id}", response_model=BookResponse)
+def update_book(book_id: int, book_update: BookCreate, db: Session = Depends(get_db)):
+    book = db.query(Book).filter(Book.id == book_id).first()
+    if not book:
+        raise HTTPException(status_code=404, detail="Book not found")
+
+    book.title = book_update.title
+    book.author = book_update.author
+    book.read = book_update.read
+
+    db.commit()
+    db.refresh(book)
+    return book
+
+
+@app.delete("/books/{book_id}", status_code=204)
+def delete_book(book_id: int, db: Session = Depends(get_db)):
+    book = db.query(Book).filter(Book.id == book_id).first()
+    if not book:
+        raise HTTPException(status_code=404, detail="Book not found")
+
+    db.delete(book)
+    db.commit()
